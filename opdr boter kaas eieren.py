dbed440189e2eddcdcedea1326d0484c53f44b11
yourinogwat <45482503+yourinogwat@users.noreply.github.com>
2021-04-05 23:00:43 +0200

    Add files via upload
    een begin

--- opdr boter kaas eieren.py
+++ opdr boter kaas eieren.py
@@ -5,6 +5,13 @@ class MyRandomAgent(EvaluationAgent):
                     def evaluate(self, board, my_symbol, opponent_symbol):
                         return random.randint(1,500)
                     
-            
+class A_God(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        getal = 1
+        if board[4] == my_symbol:
+            getal = getal + 6
+        return getal
+    
+God = A_God()    
 stupid = MyRandomAgent()
-start(player_o=stupid,player_x=stupid)
+start(player_o=God,player_x=stupid)
